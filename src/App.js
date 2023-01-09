a066974be249bb581bdf54f5df466ce65c906ba7
Jason Power <20076537@mail.wit.ie>
2023-01-09 22:12:09 +0000

    Added material UI

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import RegistrationPage from "./pages/registrationPage";
-
+import "./css/App.css"
+import {CssBaseline, Grid} from "@material-ui/core"
 function App() {
-  return (
-      <Router>
-          <div>
-              <div>
-                  <Routes>
-                      <Route path="/" element={<HomePage />} />
-                      <Route path="/login" element={<LoginPage />} />
-                      <Route path="/register" element={<RegistrationPage />} />
-                  </Routes>
-              </div>
-          </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Grid container direction={"column"}>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegistrationPage />} />
+                    </Routes>
+                </Grid>
+            </div>
+            <CssBaseline />
+        </Router>
+    );
 }
 
 export default App;
