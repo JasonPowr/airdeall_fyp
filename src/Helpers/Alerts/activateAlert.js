8d00bbf7c4e08b600bec6a8c3350f7100002e8e7
Jason Power <20076537@mail.wit.ie>
2023-03-05 09:23:40 +0000

    added reocurring location updates

--- src/Helpers/Alerts/activateAlert.js
+++ src/Helpers/Alerts/activateAlert.js
@@ -3,8 +3,9 @@ import {toggleFlashlightOff, toggleFlashlightOn} from "../Camera/camera";
 import {getLocation} from "../Maps/maps";
 
 let alertCountdown;
-const audio = new Audio(sound)
 let flashlightTrigger;
+let locationUpdates;
+const audio = new Audio(sound)
 
 export const FireAlertWithCountdown = ({alert}) => {
 
@@ -13,7 +14,7 @@ export const FireAlertWithCountdown = ({alert}) => {
         console.log(alert.title)
 
         if (alert.sms) {
-            sendSMS(alert.messageBody, alert.contact_1_phone, alert.contact_2_phone, alert.contact_3_phone, alert.locationInfo, alert.recurringLocationInfo)
+            configureSMS(alert.messageBody, alert.contact_1_phone, alert.contact_2_phone, alert.contact_3_phone, alert.locationInfo, alert.recurringLocationInfo)
         }
 
         if (alert.alarm) {
@@ -33,7 +34,7 @@ export const FireAlertWithCountdown = ({alert}) => {
 export const FireAlertWithoutCountdown = ({alert}) => {
 
     if (alert.sms) {
-        sendSMS(alert.messageBody, alert.contact_1_phone, alert.contact_2_phone, alert.contact_3_phone, alert.locationInfo, alert.recurringLocationInfo)
+        configureSMS(alert.messageBody, alert.contact_1_phone, alert.contact_2_phone, alert.contact_3_phone, alert.locationInfo, alert.recurringLocationInfo)
     }
 
     if (alert.alarm) {
@@ -48,6 +49,7 @@ export const FireAlertWithoutCountdown = ({alert}) => {
 
 export const CancelAlert = ({alert}) => {
     clearTimeout(alertCountdown);
+    clearTimeout(locationUpdates)
     toggleFlashlightOff()
 
     if (alert.flashlight) {
@@ -68,7 +70,42 @@ const validateNumber = (phoneNumber) => {
     }
     return validatedPhoneNumber
 }
-const sendSMS = async (messageBody, contact_1_phone, contact_2_phone, contact_3_phone, locationInfo, reccuringLocationInfo) => {
+
+const sendSMS = async (messageBody, contact_1_phone, contact_2_phone, contact_3_phone) => {
+    if (contact_1_phone !== "") {
+        const contact1 = await validateNumber(contact_1_phone);
+        const url = process.env.REACT_APP_FIREBASE_FUNCTION_SEND_SMS_CONTACT_1 + `${messageBody}&variable2=${contact1}`;
+        const requestOptions = {
+            method: 'GET',
+            mode: 'no-cors'
+        };
+        fetch(url, requestOptions)
+            .catch(error => console.error(error));
+    }
+
+    if (contact_2_phone !== "") {
+        const contact2 = await validateNumber(contact_2_phone);
+        const url = process.env.REACT_APP_FIREBASE_FUNCTION_SEND_SMS_CONTACT_2 + `${messageBody}&variable2=${contact2}`;
+        const requestOptions = {
+            method: 'GET',
+            mode: 'no-cors'
+        };
+        fetch(url, requestOptions)
+            .catch(error => console.error(error));
+    }
+
+    if (contact_3_phone !== "") {
+        const contact3 = await validateNumber(contact_3_phone);
+        const url = process.env.REACT_APP_FIREBASE_FUNCTION_SEND_SMS_CONTACT_3 + `${messageBody}&variable2=${contact3}`;
+        const requestOptions = {
+            method: 'GET',
+            mode: 'no-cors'
+        };
+        fetch(url, requestOptions)
+            .catch(error => console.error(error));
+    }
+}
+const configureSMS = async (messageBody, contact_1_phone, contact_2_phone, contact_3_phone, locationInfo, recurringLocationInfo) => {
 
     if (messageBody === "") {
         messageBody = "Default Alert Message"
@@ -79,22 +116,14 @@ const sendSMS = async (messageBody, contact_1_phone, contact_2_phone, contact_3_
         messageBody = messageBody + ` Alert Fired at this location: https://maps.google.com/?q=${location.lat},${location.lng}`
     }
 
-    if (contact_1_phone !== "") {
-        const contact1 = await validateNumber(contact_1_phone);
-        fetch(process.env.REACT_APP_FIREBASE_FUNCTION_SEND_SMS_CONTACT_1 + `${messageBody}&variable2=${contact1}`)
-            .catch(err => console.error(err))
-    }
+    await sendSMS(messageBody, contact_1_phone, contact_2_phone, contact_3_phone)
 
-    if (contact_2_phone !== "") {
-        const contact2 = await validateNumber(contact_2_phone);
-        fetch(process.env.REACT_APP_FIREBASE_FUNCTION_SEND_SMS_CONTACT_2 + `${messageBody}&variable2=${contact2}`)
-            .catch(err => console.error(err))
-    }
-
-    if (contact_3_phone !== "") {
-        const contact3 = await validateNumber(contact_3_phone);
-        fetch(process.env.REACT_APP_FIREBASE_FUNCTION_SEND_SMS_CONTACT_3 + `${messageBody}&variable2=${contact3}`)
-            .catch(err => console.error(err))
+    if (recurringLocationInfo) {
+        locationUpdates = setInterval(async function () {
+            const location = await getLocation()
+            messageBody = ` Location Update: https://maps.google.com/?q=${location.lat},${location.lng}`
+            await sendSMS(messageBody, contact_1_phone, contact_2_phone, contact_3_phone)
+        }, 30000);
     }
 }
 
