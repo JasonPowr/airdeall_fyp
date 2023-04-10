e229fc0d18ee2fb4b8419f853871123d271bd5a6
Jason Power <20076537@mail.wit.ie>
2023-04-10 16:17:27 +0100

    app now listens when the user loads incase phrase is spoken

---
+++ src/components/Alerts/Alerts.js
@@ -0,0 +1,138 @@
+import React, {useContext, useEffect, useRef, useState} from "react";
+import {auth} from "../../firebase";
+import {getUserAlertsFromDB} from "../../model/db/DB";
+import {ListeningButton} from "../Buttons/ListeningButton/ListeningButton";
+import {WarningPopUp} from "../Popup/WarningPopUp/WarningPopUp";
+import AlertCard from "../Cards/AlertCard/AlertCard";
+import {AlertDialog} from "../Popup/AlertPopUp/alertPopup";
+import UserContext from "../../contexts/Auth/authContext";
+import {HandleVoiceActivationOnLoad, stopTranscribing} from "../SpeechRecognition/SpeechRecognition";
+
+function Alerts() {
+    const {user} = useContext(UserContext)
+    const [alerts, setAlerts] = useState([]);
+    const [activeAlert, setActiveAlert] = useState(null);
+    const [isAlertActive, setIsAlertActive] = useState(false);
+    const [isAlertInCountdown, setIsAlertInCountdown] = useState(false);
+    const [isListening, setIsListening] = useState(true);
+    const [emailVerified, setEmailVerified] = useState(false);
+    const [phoneVerified, setPhoneVerified] = useState(false);
+    const transScriptTimeoutRef = useRef(null);
+
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                getUserAlertsFromDB().then(alerts => {
+                    setAlerts(alerts)
+                })
+                if (user.emailVerified) {
+                    setEmailVerified(true)
+                }
+                if (user.phoneNumber !== null) {
+                    setPhoneVerified(true)
+                }
+            }
+        })
+    }, []);
+
+
+    useEffect(() => {
+        const hasListenedOnLoad = sessionStorage.getItem('listenOnLoad');
+        if (alerts.length > 0) {
+            if (isListening && hasListenedOnLoad) {
+                HandleVoiceActivationOnLoad(alerts, setIsAlertActive, setIsAlertInCountdown)
+                transScriptTimeoutRef.current = setTimeout(() => {
+                    setIsListening(false)
+                    clearTimeout(transScriptTimeoutRef.current)
+                    stopTranscribing()
+                }, 10000)
+            } else {
+                clearTimeout(transScriptTimeoutRef.current)
+                stopTranscribing()
+            }
+        }
+    }, [alerts, isListening]);
+
+
+    useEffect(() => {
+        const hasListenedOnLoad = sessionStorage.getItem('listenOnLoad');
+        if (!hasListenedOnLoad && (alerts.length > 0)) {
+            listenOnLoad();
+        }
+    }, [alerts, isListening]);
+
+
+    function listenOnLoad() {
+        if (isListening) {
+            HandleVoiceActivationOnLoad(alerts, setIsAlertActive, setIsAlertInCountdown)
+            transScriptTimeoutRef.current = setTimeout(() => {
+                setIsListening(false)
+                sessionStorage.setItem('listenOnLoad', true);
+                clearTimeout(transScriptTimeoutRef.current)
+                stopTranscribing()
+            }, 10000)
+        } else {
+            clearTimeout(transScriptTimeoutRef.current)
+            stopTranscribing()
+        }
+    }
+
+    useEffect(() => {
+        if (isAlertActive || isAlertInCountdown) {
+            alerts.map((index) => {
+                if (index.alert.isActive || index.alert.isInCountdown) {
+                    setActiveAlert(index.alert)
+                }
+            })
+        }
+    }, [alerts, isAlertActive, isAlertInCountdown]);
+
+    const handleCloseEmailError = () => {
+        setEmailVerified(false);
+        return false
+    };
+
+    const handleClosePhoneError = () => {
+        setPhoneVerified(false);
+        return false
+    };
+
+    return (
+        <div>
+            <ListeningButton
+                isListening={isListening}
+                setIsListening={setIsListening}/>
+
+            <div>
+                {(user && !emailVerified) &&
+                    <WarningPopUp message={"Email is not verified"} onCloseClick={handleCloseEmailError}
+                                  context={"email"}/>}
+
+                {(user && !phoneVerified) &&
+                    <WarningPopUp message={"Phone is not verified"} onCloseClick={handleClosePhoneError}
+                                  context={"phone"}/>}
+            </div>
+
+            {(isAlertActive || isAlertInCountdown) && (
+                <AlertDialog alert={activeAlert}
+                             isAlertActive={isAlertActive}
+                             setIsAlertActive={setIsAlertActive}
+                             isAlertInCountdown={isAlertInCountdown}
+                             setAlertInCountdown={setIsAlertInCountdown}/>
+            )}
+
+            <div className={"alertContainer"}>
+                {alerts.map((index) =>
+                    <AlertCard key={index.alert.id}
+                               alert={index.alert}
+                               isAlertActive={isAlertActive}
+                               setIsAlertActive={setIsAlertActive}
+                               setAlertInCountdown={setIsAlertInCountdown}/>)}
+
+            </div>
+        </div>
+    );
+}
+
+export default Alerts;
--- src/pages/Alerts/alertpage/alertPage.js
+++ src/pages/Alerts/alertpage/alertPage.js
@@ -1,135 +1,24 @@
-import {Link} from "react-router-dom";
-import React, {useEffect, useRef, useState} from "react";
-import {Button} from "@material-ui/core";
+import React, {useState} from "react";
 import "./alertPage.css"
-import BottomNav from "../../../components/bottomNav/bottomNav";
 import {requestCameraAccess} from "../../../components/Camera/camera";
 import {requestLocationPermission} from "../../../components/Maps/maps";
-import {getUserAlertsFromDB} from "../../../model/db/DB";
-import AlertCard from "../../../components/Cards/AlertCard/AlertCard";
-import {auth} from "../../../firebase";
-import {WarningPopUp} from "../../../components/Popup/WarningPopUp/WarningPopUp";
-import {AlertDialog} from "../../../components/Popup/AlertPopUp/alertPopup";
-import {ListeningButton} from "../../../components/Buttons/ListeningButton/ListeningButton";
-import {HandleVoiceActivationOnLoad, stopTranscribing} from "../../../components/SpeechRecognition/SpeechRecognition";
+import {Link} from "react-router-dom";
+import {Button} from "@material-ui/core";
+import BottomNav from "../../../components/bottomNav/bottomNav";
+import Alerts from "../../../components/Alerts/Alerts";
 
 function AlertsPage() {
-    const [alerts, setAlerts] = useState([]);
-    const [activeAlert, setActiveAlert] = useState(null);
-    const [isAlertActive, setIsAlertActive] = useState(false);
-    const [isAlertInCountdown, setIsAlertInCountdown] = useState(false);
-    const [isListening, setIsListening] = useState(true);
-
-    const [emailVerified, setEmailVerified] = useState(false);
-    const [phoneVerified, setPhoneVerified] = useState(false);
-    const [currentUser, setCurrentUser] = useState(false);
-
-    const transScriptTimeoutRef = useRef(null);
-
     const [tab, setTab] = useState(0)
 
     requestCameraAccess()
     requestLocationPermission()
 
-
-    useEffect(() => {
-        auth.onAuthStateChanged(user => {
-            if (user) {
-                setCurrentUser(user)
-                getUserAlertsFromDB().then(alerts => {
-                    setAlerts(alerts)
-                })
-                if (user.emailVerified) {
-                    setEmailVerified(true)
-                }
-                if (user.phoneNumber !== null) {
-                    setPhoneVerified(true)
-                }
-            }
-        })
-    }, []);
-
-    useEffect(() => {
-        if (alerts.length > 0) {
-            if (isListening) {
-                HandleVoiceActivationOnLoad(alerts, setIsAlertActive, setIsAlertInCountdown)
-                transScriptTimeoutRef.current = setTimeout(() => {
-                    stopTranscribing()
-                    setIsListening(false)
-                }, 10000)
-            }
-
-            if (!isListening) {
-                stopTranscribing()
-                clearTimeout(transScriptTimeoutRef.current);
-            }
-        }
-    }, [alerts, isListening]);
-
-    useEffect(() => {
-        if (isAlertActive || isAlertInCountdown) {
-            alerts.map((index) => {
-                if (index.alert.isActive || index.alert.isInCountdown) {
-                    setActiveAlert(index.alert)
-                }
-            })
-        }
-    }, [alerts, isAlertActive, isAlertInCountdown]);
-
-    const handleCloseEmailError = () => {
-        setEmailVerified(false);
-        return false
-    };
-
-    const handleClosePhoneError = () => {
-        setPhoneVerified(false);
-        return false
-    };
-
     return (
         <div>
-            {currentUser ? (
-                <div>
-                    <header>Your Alerts</header>
-
-                    <ListeningButton
-                        isListening={isListening}
-                        setIsListening={setIsListening}/>
-
-                    <div>
-                        {(currentUser && !emailVerified) &&
-                            <WarningPopUp message={"Email is not verified"} onCloseClick={handleCloseEmailError}
-                                          context={"email"}/>}
-
-                        {(currentUser && !phoneVerified) &&
-                            <WarningPopUp message={"Phone is not verified"} onCloseClick={handleClosePhoneError}
-                                          context={"phone"}/>}
-                    </div>
-
-                    {(isAlertActive || isAlertInCountdown) && (
-                        <AlertDialog alert={activeAlert}
-                                     isAlertActive={isAlertActive}
-                                     setIsAlertActive={setIsAlertActive}
-                                     isAlertInCountdown={isAlertInCountdown}
-                                     setAlertInCountdown={setIsAlertInCountdown}/>
-                    )}
-
-                    <div className={"alertContainer"}>
-                        {alerts.map((index) =>
-                            <AlertCard key={index.alert.id}
-                                       alert={index.alert}
-                                       isAlertActive={isAlertActive}
-                                       setIsAlertActive={setIsAlertActive}
-                                       setAlertInCountdown={setIsAlertInCountdown}/>)}
-                    </div>
-
-                    <Link to={"/create_alert"}><Button>Create Alert</Button></Link>
-                    <BottomNav value={tab} onChange={setTab}/>
-                </div>
-
-            ) : (
-                <div>Loading .....</div>
-            )}
+            <h1> Your Alerts </h1>
+            <Alerts/>
+            <Link to={"/create_alert"}><Button>Create Alert</Button></Link>
+            <BottomNav value={tab} onChange={setTab}/>
         </div>
     );
 }
